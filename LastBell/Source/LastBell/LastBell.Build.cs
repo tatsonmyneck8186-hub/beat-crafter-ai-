7a33da62304d34f3afbd69af1165898276dce192
tatsonmyneck8186-hub <tatsonmyneck8186@gmail.com>
2026-06-19 18:02:06 -0400

    feat: Add LAST BELL UE5.7 project config and build system

---
+++ LastBell/Source/LastBell/LastBell.Build.cs
@@ -0,0 +1,30 @@
+using UnrealBuildTool;
+
+public class LastBell : ModuleRules
+{
+    public LastBell(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(new string[]
+        {
+            "Core",
+            "CoreUObject",
+            "Engine",
+            "InputCore",
+            "EnhancedInput",
+            "UMG",
+            "AIModule",
+            "NavigationSystem",
+            "GameplayTasks",
+            "SlateCore",
+            "Slate",
+            "PhysicsCore",
+        });
+
+        PrivateDependencyModuleNames.AddRange(new string[]
+        {
+            "NetCore",
+        });
+    }
+}
